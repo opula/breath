a390ba788ed3a15f3fa9effcf8862eb0a56b2865
Aaron Marz <aaron.marz@gmail.com>
2025-04-18 23:09:45 -0400

    Before audio change.

--- src/constants/music.web.ts
+++
@@ -1,4 +0,0 @@
-import {IOSCategoryOptions} from '@5stones/react-native-track-player';
-
-export const DEFAULT_IOS_CATEGORIES = [] as IOSCategoryOptions[];
-export const MIX_IOS_CATEGORIES = [IOSCategoryOptions.MixWithOthers];
